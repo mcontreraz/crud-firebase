891b019f03d9c5f17a86649dee8dfda88bd0c87b
Marcos Contreras <mcontreras@acl.cl>
2021-05-31 09:46:33 -0400

    test desde mac

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ function App() {
   };
   return (
     <div className="container mt-3">
-      <h1 className="text-center">ToDo List test github v3</h1>
+      <h1 className="text-center">ToDo List test github desde mac</h1>
       <div className="row">
         <div className="col-md-6">
           <h3>Lista tareas</h3>
